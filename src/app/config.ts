a3cbfb901a1748f5ec0c477ef6a970e26937c61b
Dimiter Jilanov <d.jilanov@mdg.bg>
2016-11-15 12:20:54 +0200

    Update the ports to point at Heroku and update nodejs of the back-end to latest

--- src/app/config.ts
+++ src/app/config.ts
@@ -8,11 +8,12 @@ export class Config {
     public static get languages():Array<string> { return ['bg', 'en']; }
     public static get hearthBeatIntervalInSeconds():number { return 20; }
     // urls
-    public static get usersUrl():string { return "http://4055c814.ngrok.io/api/users"; }
-    public static get heartbeatUrl():string { return "http://4055c814.ngrok.io/api/heartbeat"; }
-    public static get changeUsersUrl():string { return "http://4055c814.ngrok.io/api/users/{{id}}"; }
+    // public static get usersUrl():string { return "http://4055c814.ngrok.io/api/users"; }
+    // public static get heartbeatUrl():string { return "http://4055c814.ngrok.io/api/heartbeat"; }
+    // public static get changeUsersUrl():string { return "http://4055c814.ngrok.io/api/users/{{id}}"; }
 
     // staging
-    // public static get usersUrl():string { return "https://morning-oasis-39757.herokuapp.com/api/users"; }
-    // public static get changeUsersUrl():string { return "https://morning-oasis-39757.herokuapp.com/api/users/{{id}}"; }
+    public static get usersUrl():string { return "https://morning-oasis-39757.herokuapp.com/api/users"; }
+    public static get heartbeatUrl():string { return "https://morning-oasis-39757.herokuapp.com/api/heartbeat"; }
+    public static get changeUsersUrl():string { return "https://morning-oasis-39757.herokuapp.com/api/users/{{id}}"; }
 }
